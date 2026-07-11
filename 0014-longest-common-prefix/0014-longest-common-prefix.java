c51bc7df2d5d4535a4c2adabf3ab0079b9529a37
Shaik Yunis <166291506+Yunis147@users.noreply.github.com>
2026-07-11 19:44:41 +0530

    Time: 1 ms (62.64%), Space: 43.4 MB (17.29%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,15 +1,25 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder res = new StringBuilder();
-        Arrays.sort(strs);
-        char[] first = strs[0].toCharArray();
-        char[] last = strs[strs.length-1].toCharArray();
-        for(int i=0;i<first.length;i++){
-            if(first[i]!=last[i]){
-                break;
+        String first = strs[0];
+
+        for (int i = 0; i < first.length(); i++) {
+            char ch = first.charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+
+                // Current string ended
+                if (i == strs[j].length()) {
+                    return first.substring(0, i);
+                }
+
+                // Characters don't match
+                if (ch != strs[j].charAt(i)) {
+                    return first.substring(0, i);
+                }
             }
-            res.append(first[i]);
         }
-        return res.toString();
+
+        // Entire first string is the common prefix
+        return first;
     }
 }
