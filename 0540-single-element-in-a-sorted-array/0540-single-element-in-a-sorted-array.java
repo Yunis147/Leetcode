3d1dfe3299be8aebed58f2eac06818771e395caf
Shaik Yunis <166291506+Yunis147@users.noreply.github.com>
2026-07-14 19:16:39 +0530

    Time: 0 ms (100%), Space: 53.1 MB (21.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,11 +2,8 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
      int l=0;
      int r=nums.length-1;
-     while(l<=r){
+     while(l<r){
         int mid=(l+r)/2;
-         if(l==r){
-            return nums[l];
-         }
         if(nums[mid]==nums[mid-1]){
             if((r-mid)%2==0){
                 r=mid-2;
@@ -23,6 +20,6 @@ class Solution {
             return nums[mid];
         }
      }
-     return -1;
+     return nums[l];
     }
 }
