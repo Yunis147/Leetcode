2b79e0afffc490187858c56558063bc4af3ec24f
Shaik Yunis <166291506+Yunis147@users.noreply.github.com>
2026-07-22 23:32:20 +0530

    Time: 1 ms (99.13%), Space: 42.9 MB (30.34%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,14 +1,9 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        HashMap<Integer,Integer> m = new HashMap<>();
-        while(n!=0){
-            int ld = n%10;
-            n=n/10;
-            m.put(ld,m.getOrDefault(ld,0)+1);
-        }
         int sum=0;
-        for(Integer i:m.keySet()){
-            sum+= i*m.get(i);
+        while(n!=0){
+            sum+= n%10;
+            n/=10;
         }
         return sum;
     }
