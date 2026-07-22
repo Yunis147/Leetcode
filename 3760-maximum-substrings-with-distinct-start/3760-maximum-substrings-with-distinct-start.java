d82b5443575454795201180e5e84891dc9f40822
Shaik Yunis <166291506+Yunis147@users.noreply.github.com>
2026-07-22 23:41:06 +0530

    Time: 6 ms (92.03%), Space: 47.6 MB (89.21%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -1,9 +1,15 @@
 class Solution {
     public int maxDistinct(String s) {
-        HashSet<Character> set = new HashSet<>();
-        for(char ch:s.toCharArray()){
-            set.add(ch);
+        boolean[] seen = new boolean[26];
+        int count = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (!seen[ch - 'a']) {
+                seen[ch - 'a'] = true;
+                count++;
+            }
         }
-        return set.size();
+
+        return count;
     }
 }
