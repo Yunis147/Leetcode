3b5ec753d50d06b22f07f429ad62730741be99f8
Shaik Yunis <166291506+Yunis147@users.noreply.github.com>
2026-07-30 21:37:20 +0530

    Time: 1 ms (77.29%), Space: 46.7 MB (43.61%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,8 @@ class Solution {
        int j=0;
        for(int i=0;i<nums.length;i++){
         if(nums[j]!=nums[i]){
-            nums[++j]=nums[i];
+            j++;
+            nums[j]=nums[i];
         }
        }
        return j+1;
